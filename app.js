40dc425d9dadb88b9af4de4d409d1401a4860726
Victor Carpenter <victorcarpenter123@gmail.com>
2017-01-31 11:43:31 -0500

    it is broken rn

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //HUGE THANKS TO THE MDN tut https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
 var game;
+var horizAspect = window.innerWidth/window.innerHeight;
 
 function start (){
   var canvas = document.getElementById('canvas');
@@ -34,6 +35,7 @@ function start (){
 
 
   //clear the color and the depth tester
+  console.log(game.COLOR_BUFFER_BIT)
   game.clear(game.COLOR_BUFFER_BIT | game.DEPTH_BUFFER_BIT);
 
 
@@ -113,3 +115,55 @@ function getShader(game, id, type) {
 
   return shader;
 }
+
+function initBuffers() {
+  squareVerticesBuffer = game.createBuffer();
+  game.bindBuffer(game.ARRAY_BUFFER, squareVerticesBuffer);
+
+  var vertices = [
+    1.0,  1.0,  0.0,
+    -1.0, 1.0,  0.0,
+    1.0,  -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ];
+
+  game.bufferData(game.ARRAY_BUFFER, new Float32Array(vertices), game.STATIC_DRAW);
+}
+
+function drawScene() {
+
+  game.clear(game.COLOR_BUFFER_BIT | game.DEPTH_BUFFER_BIT);
+
+  perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+
+  loadIdentity();
+  mvTranslate([-0.0, 0.0, -6.0]);
+
+  game.bindBuffer(game.ARRAY_BUFFER, squareVerticesBuffer);
+  game.vertexAttribPointer(vertexPositionAttribute, 3, game.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  game.drawArrays(game.TRIANGLE_STRIP, 0, 4);
+}
+
+function loadIdentity() {
+  mvMatrix = Matrix.I(4);
+}
+
+function multMatrix(m) {
+  mvMatrix = mvMatrix.x(m);
+}
+
+function mvTranslate(v) {
+  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms() {
+  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
+
+drawScene()
